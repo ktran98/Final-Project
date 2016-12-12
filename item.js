e27a71cd29988a446b993bc3039c43f42d590bb1
ktran98 <peterkhanhtran@gmail.com>
2016-12-12 13:55:13 -0600

    Add files via upload

---
+++ item.js
@@ -0,0 +1,25 @@
+class Item{
+  //Fill in the item class below this comment
+  constructor(name,price,shipping){
+    this.name = name;
+    this.price = price;
+    this.shipping = shipping;
+  };
+}
+
+//Create your three test items below this comment.
+this.doll = new Item{
+  this.name = Teddy;
+  this.price = 25;
+  this.shipping = 2;
+};
+this.game = new Item{
+  this.name = pokemon;
+  this.price = 30;
+  this.shipping = 3;
+};
+this.toy = new Item{
+  this.name = mini car;
+  this.price = 15;
+  this.shipping = 1;
+}
