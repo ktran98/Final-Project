f6fe9830cfa2f8857dbfe40e45ee99e63cb3b9c1
ktran98 <peterkhanhtran@gmail.com>
2016-12-14 14:02:14 -0600

    Add files via upload

--- password.js
+++ password.js
@@ -1,12 +1,13 @@
 class Password{
   //The first part of the class.
 constructor(publickey, privatekey){
-  this.publickey = 
+  this.publickey = [];
+  this.privatekey = [];
 }
-
-
   //Instance functions below this comment.
-
+  function validPublickey(){
+    if publickey =
+  }
 
 
   //Static function below this comment.
